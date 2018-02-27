cb4eab0e816f600fe74f5dfeeb1afae29ce42ee0
Laurynas B <l.butrimavicius@gmail.com>
2018-02-27 19:40:15 +0200

    New

--- Projektas2 - celc to fahr to kelv/script/js.js
+++ Projektas2 - celc to fahr to kelv/script/js.js
@@ -19,17 +19,28 @@ var celcius, kelvin, fahrenheit;
         fahrenheit.on ('input', function () {
           celci =  celcius.val(((parseFloat(this.value) - 32) / 1.8).toFixed(2));
           kelvi = kelvin.val(((parseFloat(this.value) + 459.67) * 5/9).toFixed(2));
-
         });
 
 
-// kelvin.value = (parseFloat(this.value) + 273.15).toFixed(2);
-// fahrenheit.value = (parseFloat(this.value) * 1.8 + 32).toFixed(2);
+//var celcius, kelvin, fahrenheit;
 
+//{
+   // celcius = document.getElementById("celc");
+   // kelvin = document.getElementById("kelv");
+   // fahrenheit = document.getElementById("fahr");
+//}
 
-// celcius.value = ((parseFloat(this.value) - 32) / 1.8).toFixed(2);
-// kelvin.value = ((parseFloat(this.value) + 459.67) * 5/9).toFixed(2);
+//celcius.oninput = function () {
+    //kelvin.value = (parseFloat(celcius.value) + 273.15).toFixed(2);
+    //fahrenheit.value = (parseFloat(celcius.value) * 1.8 + 32).toFixed(2);
+//};
 
+//fahrenheit.oninput = function () {
+    //celcius.value = ((parseFloat(fahrenheit.value) - 32) / 1.8).toFixed(2);
+    //kelvin.value = ((parseFloat(fahrenheit.value) + 459.67) * 5/9).toFixed(2);
+//};
 
-// celcius.value = (parseFloat(this.value) - 273.15).toFixed(2);
-// fahrenheit.value = (parseFloat(this.value) * 9/5 - 459.67).toFixed(2);
+//kelvin.oninput = function () {
+    //celcius.value = (parseFloat(kelvin.value) - 273.15).toFixed(2);
+    //fahrenheit.value = (parseFloat(kelvin.value) * 9/5 - 459.67).toFixed(2);
+//};
