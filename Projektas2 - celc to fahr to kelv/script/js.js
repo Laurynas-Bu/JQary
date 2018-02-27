75f6a5eb4d72a63f8cc9f4b3c32b0f4134c178da
Laurynas <l.butrimavicius@gmail.com>
2018-02-27 14:34:54 +0200

    New

--- Projektas2 - celc to fahr to kelv/script/js.js
+++ Projektas2 - celc to fahr to kelv/script/js.js
@@ -7,18 +7,18 @@ var celcius, kelvin, fahrenheit;
 }
 
         celcius.on ('input', function () {
-            kelvin.val(parseFloat(this.value) + 273.15);
-            fahrenheit.val(parseFloat(this.value) * 1.8 + 32);
+            kelvin.val((parseFloat(this.value) + 273.15).toFixed(2));
+            fahrenheit.val((parseFloat(this.value) * 1.8 + 32).toFixed(2));
         });
 
         kelvin.on ('input', function () {
-            celcius.val(parseFloat(this.value) - 273.15);
-            fahrenheit.val(parseFloat(this.value) * 9/5 - 459.67);
+            celcius.val((parseFloat(this.value) - 273.15).toFixed(2));
+            fahrenheit.val((parseFloat(this.value) * 9/5 - 459.67).toFixed(2));
         });
 
         fahrenheit.on ('input', function () {
-          celci =  celcius.val((parseFloat(this.value) - 32) / 1.8);
-          kelvi = kelvin.val((parseFloat(this.value) + 459.67) * 5/9);
+          celci =  celcius.val(((parseFloat(this.value) - 32) / 1.8).toFixed(2));
+          kelvi = kelvin.val(((parseFloat(this.value) + 459.67) * 5/9).toFixed(2));
 
         });
 
