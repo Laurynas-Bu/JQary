4086432ec7dd3d590492d4a47c00a30d460df838
Laurynas B <l.butrimavicius@gmail.com>
2018-02-28 19:54:32 +0200

    New

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var minify = require('gulp-clean-css');
+var gulp_minify = require('gulp-minify');
 var cssbeautify = require('gulp-cssbeautify');
+var concat = require('gulp-concat');
+var vfs = require('vinyl-fs');
 
 gulp.task('styles', function() {
-    return gulp.src('scss/**/*.scss')
+    return gulp.src('Projektas3/scss/**/*.scss')
         .pipe(sass())
         //.pipe(minify())
         .pipe(cssbeautify())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('Projektas3/css/'));
 });
 
+gulp.task('js', function () {
+    return gulp.src(['Projektas3/script/dev/*.js', 'Projektas3/script/dev/*.min.js'])
+        .pipe(concat('jscript.js'))
+        //.pipe(gulp_minify())
+        .pipe(gulp.dest('Projektas3/script/prod'));
+});
 
 //Watch task
 gulp.task('watch',function() {
-    gulp.watch('scss/**/*.scss', gulp.series('styles'));
+    gulp.watch('Projektas3/scss/**/*.scss', gulp.series('styles'));
 });
