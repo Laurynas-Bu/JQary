aabb1fb6074f2ced5bf0fa0f9710e7e040310108
Laurynas <l.butrimavicius@gmail.com>
2018-03-06 17:02:38 +0200

    New

--- Projektas3/script/dev/js.js
+++ Projektas3/script/dev/js.js
@@ -1,22 +1,22 @@
-$(document).ready(function () {
-
-    $('#rytas').click(function () {
-        allIn();
-    });
-    function allIn() {
-        var check = $('#forma input');
-        var error = 'Užpildykite visus laukus';
-        var html ='<tr>';
-        check.each(function(i, formInput){
-            if (check.eq(i).val() === '') {
-                alert(error);
-                return false
-            }
-            html +='<td>' + check.eq(i).val() + '</td>';
-        });
-        html +='</tr>';
-        $('.table tbody').append(html);
-    }
+ $(document).ready(function () {
+//
+//     $('#rytas').click(function () {
+//         allIn();
+//     });
+//     function allIn() {
+//         var check = $('#forma input');
+//         var error = 'Užpildykite visus laukus';
+//         var html ='<tr>';
+//         check.each(function(i, formInput){
+//             if (check.eq(i).val() === '') {
+//                 alert(error);
+//                 return false
+//             }
+//             html +='<td>' + check.eq(i).val() + '</td>';
+//         });
+//         html +='</tr>';
+//         $('.table tbody').append(html);
+//     }
 
     //Datepicker
 
