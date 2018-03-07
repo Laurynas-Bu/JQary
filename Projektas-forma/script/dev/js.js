52550bf8ed29857b64da9e629792b46547d56f1a
Laurynas <l.butrimavicius@gmail.com>
2018-03-07 02:35:18 +0200

    New

--- Projektas-forma/script/dev/js.js
+++ Projektas-forma/script/dev/js.js
@@ -19,4 +19,3 @@ $(document).ready(function () {
         $('.table tbody').append(html);
     }
 });
-
--- Projektas3/script/dev/js.js
+++ Projektas3/script/dev/js.js
@@ -1,27 +1,9 @@
  $(document).ready(function () {
-//
-//     $('#rytas').click(function () {
-//         allIn();
-//     });
-//     function allIn() {
-//         var check = $('#forma input');
-//         var error = 'Užpildykite visus laukus';
-//         var html ='<tr>';
-//         check.each(function(i, formInput){
-//             if (check.eq(i).val() === '') {
-//                 alert(error);
-//                 return false
-//             }
-//             html +='<td>' + check.eq(i).val() + '</td>';
-//         });
-//         html +='</tr>';
-//         $('.table tbody').append(html);
-//     }
 
     //Datepicker
 
     var newArray = [
-        { day: [1, 2, 5, 6, 10, 12, 22], time: [
+        { days: [1, 2, 5, 6, 10, 12, 22], time: [
                 ['8:00', '8:30', '9:00', '9:30', '10:00'],
                 ['8:30', '9:00', '10:00'],
                 ['8:00', '9:00', '10:00'],
@@ -32,7 +14,7 @@
             ]
         },
         {
-            day: [5, 8, 11, 12, 14, 16, 22, 24, 25, 28], time: [
+            days: [5, 8, 11, 12, 14, 16, 22, 24, 25, 28], time: [
             ['12:00', '14:30', '15:00', '15:30'],
             ['13:00', '13:30', '14:00'],
             ['12:30', '14:00', '15:00', '15:30'],
@@ -46,28 +28,24 @@
         },
 
         {
-            day: [4 ,11, 15, 20, 22, 26, 27], time: [
-                ['16:00', '14:30', '15:00', '15:30'],
-                ['13:00', '13:30', '14:00'],
-                ['12:30', '14:00', '15:00', '15:30'],
-                ['12:00'],
-                ['13:30'],
-                [],
-                ['14:00', '15:00'],
-                ['15:30'],
-                ['15:00', '15:30']
+            days: [4 ,11, 15, 20, 22, 26, 27], time: [
+                ['18:00', '18:30'],
+                ['16:00', '16:30', '17:00'],
+                ['16:30', '17:00', '18:00', '18:30'],
+                ['16:30', '18:00'],
+                ['17:00', '18:30'],
+                ['16:00', '17:00'],
+                []
             ]
         }
     ];
-    console.log(newArray[0].time[2]);
-    var eventDates =
-            newArray[0].day,
+    console.log(newArray[1].time[6]);
+
+    var eventDates = newArray[0].days,
+        eventTime = newArray[0].time,
         $picker = $('#custom-cells'),
         $content = $('#custom-cells-events'),
-        sentences = [ 'Lorem',
-            'Lorem',
-            'Lorem',
-            'Lorem'];
+        sentences = [];
 
     $picker.datepicker({
         onRenderCell: function (date, cellType) {
@@ -79,6 +57,24 @@
                 }
             }
         },
+
+    //     $('#rytas').click(function () {
+    //     allIn();
+    // });
+    //  function allIn() {
+    //      var check = $(newArray[index].time);
+    //      var html ='<tr>';
+    //      check.each(function(i){
+    //          if (check.eq(i).val() === '') {
+    //              alert(error);
+    //              return false
+    //          }
+    //          html +='<td>' + check.eq(i).val() + '</td>';
+    //      });
+    //      html +='</tr>';
+    //      $('.table tbody').append(html);
+    //  }
+
         onSelect: function onSelect(fd, date) {
             var title = '', content = '';
             // If date with event is selected, show it
